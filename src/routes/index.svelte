d914d9004795e956e0b695cf5eb9705ac6ebb652
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-08-03 15:51:39 +0200

    Add maplibre & map component & modify prettierrc

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Map } from '$lib/components/index';
+  let center = [-122.4, 37.7];
+</script>
+
+<div class="map-section">
+  <Map lat={center[1]} lon={center[0]} recenterOnUpdate zoom="10" />
+</div>
+
+<style>
+  .map-section {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    top: 0;
+    width: 100%;
+  }
+</style>
