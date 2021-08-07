e8acda8398e7d95df81d76c191743339562212a6
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-08-07 20:38:41 +0200

    Query some wikipedia articles & ignore places without an article

--- src/lib/util/wikidata/buildQuery.ts
+++ src/lib/util/wikidata/buildQuery.ts
@@ -1,13 +1,35 @@
 export default (westCorner: Array<number>, eastCorner: Array<number>): string => {
-  return `SELECT ?place ?location ?placeLabel ?instanceLabel ?image WHERE {
-      SERVICE wikibase:box {
-          ?place wdt:P625 ?location .
-          bd:serviceParam wikibase:cornerWest "Point(${westCorner[0]} ${westCorner[1]})"^^geo:wktLiteral .
-          bd:serviceParam wikibase:cornerEast "Point(${eastCorner[0]} ${eastCorner[1]})"^^geo:wktLiteral .
-        }
-        OPTIONAL { ?place wdt:P31 ?instance }
-        FILTER NOT EXISTS {?instance wdt:P279 wd:Q34442.}
-        OPTIONAL { ?place wdt:P18 ?image. }
-        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-      }`;
+  const langs = ['fr', 'nl', 'en'];
+  const wikiArticles = langs.reduce((acc, lang) => {
+    const key = `?${lang}Article`;
+    acc[key] = `OPTIONAL {
+      ${key} schema:about ?place.
+      ${key} schema:isPartOf <https://${lang}.wikipedia.org/>.
+    }`;
+    return acc;
+  }, {});
+
+  const ignoreNoWikipedia = true;
+
+  return `
+  SELECT ?place ?location ?placeLabel ?instanceLabel ?image ${Object.keys(wikiArticles).join(
+    ' '
+  )} ?prefArticle WHERE {
+    SERVICE wikibase:box {
+      ?place wdt:P625 ?location .
+      bd:serviceParam wikibase:cornerWest "Point(${westCorner[0]} ${
+    westCorner[1]
+  })"^^geo:wktLiteral .
+        bd:serviceParam wikibase:cornerEast "Point(${eastCorner[0]} ${
+    eastCorner[1]
+  })"^^geo:wktLiteral .
+      }
+    OPTIONAL { ?place wdt:P31 ?instance }
+    FILTER NOT EXISTS {?instance wdt:P279 wd:Q34442.}
+    OPTIONAL { ?place wdt:P18 ?image. }
+    ${Object.values(wikiArticles).join('\n')}
+    BIND(COALESCE(${Object.keys(wikiArticles).join(', ')}, "") AS ?prefArticle)
+    ${ignoreNoWikipedia ? 'FILTER (?prefArticle != "")' : ''}
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "${langs.join(',')}". }
+  }`;
 };
