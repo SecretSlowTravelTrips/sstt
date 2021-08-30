390eff5247012801ee4ca44253b468fbfb6faf22
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-08-31 01:07:43 +0200

    Add Label component

--- src/lib/components/UI/index.ts
+++ src/lib/components/UI/index.ts
@@ -5,3 +5,4 @@ export { default as FormGroup } from './FormGroup.svelte';
 export { default as Details } from './Details.svelte';
 export { default as Tag } from './Tag.svelte';
 export { default as TokenizerInput } from './TokenizerInput.svelte';
+export { default as Label } from './Label.svelte';
