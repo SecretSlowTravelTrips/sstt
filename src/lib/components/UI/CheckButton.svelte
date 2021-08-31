009b2dddd92f3c001835a54f5b8f117eadce3ab6
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-08-31 13:37:09 +0200

    Update CheckButton

--- src/lib/components/UI/CheckButton.svelte
+++ src/lib/components/UI/CheckButton.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
+  import { draw } from 'svelte/transition';
+
   export let label: string;
   export let disabled = false;
   export let checked = false;
 </script>
 
 <label
-  class="cursor-pointer py-1 px-4 {checked
+  class="inline-flex items-center cursor-pointer py-1 px-2 {checked
     ? 'bg-green-300'
-    : 'bg-gray-100'} hover:bg-green-200 focus-within:outline-black"
+    : 'bg-gray-100'} hover:bg-green-200 focus-within:outline-black transition-colors"
 >
+  {#if checked}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-4 w-4 mr-2 text-yellow-800"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M5 13l4 4L19 7"
+        transition:draw
+      />
+    </svg>
+  {/if}
   <input type="checkbox" class="sr-only" {disabled} bind:checked />
   {label}
 </label>
