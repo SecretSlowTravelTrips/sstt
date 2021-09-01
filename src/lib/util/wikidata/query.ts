f213d5f57cf7000186ca26486a32063c728c03eb
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-09-02 00:15:14 +0200

    Update wikidata/query.ts

--- src/lib/util/wikidata/query.ts
+++ src/lib/util/wikidata/query.ts
@@ -1,24 +1,28 @@
-import fileToGeoJSON from '$lib/util/fileToGeoJson';
 import SPARQLQueryDispatcher from './SPARQLQueryDispatcher';
 import buildQuery from './buildQuery';
 import toGeoJSON from './toGeoJSON';
-import generateBuffer from '$lib/util/generateBuffer';
-import { AllGeoJSON, bbox, FeatureCollection, Point, Properties, within } from '@turf/turf';
+import {
+  bbox,
+  FeatureCollection,
+  MultiPolygon,
+  Point,
+  Polygon,
+  Properties,
+  within
+} from '@turf/turf';
 
 export default async (
-  file,
-  radius = 1,
+  file: File,
+  buffer: FeatureCollection<Polygon | MultiPolygon>,
   langs: string[]
 ): Promise<FeatureCollection<Point, Properties>> => {
   const queryDispatcher = new SPARQLQueryDispatcher();
-  const createdGeoJson: AllGeoJSON = await fileToGeoJSON(file);
 
-  const polygon = generateBuffer(createdGeoJson, radius);
-  const bbx = bbox(polygon);
+  const bbx = bbox(buffer);
 
   const data = within(
     toGeoJSON(await queryDispatcher.query(buildQuery([bbx[0], bbx[1]], [bbx[2], bbx[3]], langs))),
-    polygon
+    buffer
   );
   return data;
 };
