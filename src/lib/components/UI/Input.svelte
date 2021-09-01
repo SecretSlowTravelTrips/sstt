6188cebb66e53467a4520d48be55e1470c2f8af7
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-09-01 19:25:52 +0200

    Update Input

--- src/lib/components/UI/Input.svelte
+++ src/lib/components/UI/Input.svelte
@@ -4,6 +4,13 @@
   export let id: string = name;
   export let max: number = undefined;
   export let value;
+
+  const handleInput = (e) => {
+    // https://stackoverflow.com/a/57393751
+    // in here, you can switch on type and implement
+    // whatever behaviour you need
+    value = type.match(/^(number|range)$/) ? +e.target.value : e.target.value;
+  };
 </script>
 
-<input {type} {name} {value} {max} {id} class="px-3 py-2 rounded w-full" />
+<input {type} {name} {value} {max} {id} class="px-3 py-2 rounded w-full" on:input={handleInput} />
