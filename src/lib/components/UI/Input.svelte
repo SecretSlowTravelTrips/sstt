25b1887c1e5e04e41922e36b83a2ae1db942eb35
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-09-01 20:19:46 +0200

    Update Input

--- src/lib/components/UI/Input.svelte
+++ src/lib/components/UI/Input.svelte
@@ -7,8 +7,6 @@
 
   const handleInput = (e) => {
     // https://stackoverflow.com/a/57393751
-    // in here, you can switch on type and implement
-    // whatever behaviour you need
     value = type.match(/^(number|range)$/) ? +e.target.value : e.target.value;
   };
 </script>
