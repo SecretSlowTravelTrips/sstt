1566715ca55e0a012bcb963d9273061de6d28f0c
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-09-12 19:45:58 +0200

    Move overpass related UI into an Overpass.svelte component

---
+++ src/lib/components/Overpass.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { config } from '$lib/util/overpass';
+  import { LoadButton as DownloadButton, Details } from '$lib/components/UI';
+
+  export let overpassService;
+</script>
+
+<Details summary="Overpass">
+  <div class="flex flex-wrap gap-1">
+    {#each config.overpassQueryButtons as { query, name } (name)}
+      <DownloadButton
+        on:click={() => overpassService.send('DOWNLOAD_DATA', { query, name })}
+        {name}
+      >
+        {name}
+      </DownloadButton>
+    {/each}
+  </div>
+</Details>
