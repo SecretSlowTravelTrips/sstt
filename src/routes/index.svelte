1bc6e6ed8263379a687ff319c1c3cb10ade48ed1
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2021-09-12 19:46:50 +0200

    Use state machines & updated/ created components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
   import { Map, Trail } from '$lib/components/Map';
-  import { queryAndDownload, config } from '$lib/util/overpass';
-  import { fileToGeoJSON, removeFilenameExtention } from '$lib/util';
-  import {
-    Input,
-    FileInput,
-    LoadButton as DownloadButton,
-    FormGroup,
-    Details
-  } from '$lib/components/UI';
-  import Wikidata from '$lib/components/Wikidata.svelte';
+  import { Input, FileInput, FormGroup } from '$lib/components/UI';
+  import { Wikidata } from '$lib/components/Wikidata';
+  import { useMachine, useSelector } from '@xstate/svelte';
+  import { appMachine } from '$lib/machines';
+  import Overpass from '$lib/components/Overpass.svelte';
+
+  const { state, send, service } = useMachine(appMachine);
+  const wikiService = useSelector(service, (state) => state.children.wikiMachine);
+  const overpassService = useSelector(service, (state) => state.children.overpassMachine);
 
   let center = {
     lat: 51,
@@ -18,60 +17,41 @@
   let files: FileList;
   let radiusInM = 1000;
   let radiusInKm: number;
-  let uploadedTrail;
+  let timeoutID;
 
   const maxRadius = 50000;
 
   $: {
-    if (radiusInM < 1) radiusInM = 1;
-    else if (radiusInM > maxRadius) radiusInM = maxRadius;
-
     radiusInKm = radiusInM / 1000;
+    if (radiusInM <= 0) radiusInKm = 0.001;
+    else if (radiusInM > maxRadius) radiusInKm = 50;
+    clearTimeout(timeoutID);
+    timeoutID = setTimeout(() => send('UPDATE_RADIUS', { radius: radiusInKm }), 500);
   }
+
+  $: if (files) send('FILE_UPLOAD', { file: files[0] });
 </script>
 
 <main class="w-full h-full flex">
   <div class="w-60 md:w-96 flex flex-col p-2 max-h-full overflow-y-auto flex-shrink-0">
     <FormGroup>
-      <FileInput
-        bind:files
-        on:change={async () => (uploadedTrail = await fileToGeoJSON(files[0]))}
-      />
+      <FileInput bind:files />
     </FormGroup>
-    <FormGroup>
-      <label for="radius">
-        Radius (meters)
-        <Input type="number" id="radius" bind:value={radiusInM} max={maxRadius} />
-      </label>
-      <small slot="help">Only values between 1 and 50000 are allowed</small>
-    </FormGroup>
-    {#if files && files[0]}
-      <Details summary="Overpass">
-        <div class="flex flex-wrap gap-1">
-          {#each config.overpassQueryButtons as configuration, i}
-            <DownloadButton
-              on:click={() =>
-                queryAndDownload(
-                  files[0],
-                  configuration.query,
-                  `${removeFilenameExtention(files[0].name)}---${radiusInM}m---${
-                    configuration.name
-                  }`,
-                  radiusInKm
-                )}
-              name={configuration.name}
-            >
-              {configuration.name}
-            </DownloadButton>
-          {/each}
-        </div>
-      </Details>
-      <Wikidata radius={radiusInKm} {files} />
+    {#if $state.value === 'query'}
+      <FormGroup>
+        <label for="radius">
+          Radius (meters)
+          <Input type="number" id="radius" bind:value={radiusInM} max={maxRadius} />
+        </label>
+        <small slot="help">Only values between 1 and 50000 are allowed</small>
+      </FormGroup>
+      <Overpass overpassService={$overpassService} />
+      <Wikidata wikiService={$wikiService} />
     {/if}
   </div>
   <Map initialLat={center.lat} initialLon={center.lon} initialZoom={7}>
-    {#if uploadedTrail}
-      <Trail trail={uploadedTrail} />
+    {#if $state.context.geojson}
+      <Trail trail={$state.context.geojson} />
     {/if}
   </Map>
 </main>
